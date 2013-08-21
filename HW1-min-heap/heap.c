6bb01886d35afd55f8b25b15fc7a68bf0cba6496
Ryan Quackenbush <quackenbushrw@gmail.com>
2013-08-20 18:19:21 -0700

    Implemented printList, getMin, leftChildIndex, rightChildIndex, and heapify functions

--- HW1-min-heap/heap.c
+++ HW1-min-heap/heap.c
@@ -1,17 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "heap.h"
 
-void heapify( int A[], int i, int n){
+int leftChildIndex( int index ){
+	return 2 * ( index + 1 ) - 1;
+}
 
+int rightChildIndex( int index ){
+	return 2 * ( index + 1 );
+}
+
+void heapify( int A[], int index, int len ){
+	int leftChild, rightChild, newIndex, swapVal;
+
+	leftChild = leftChildIndex( index );
+	rightChild = rightChildIndex( index );
+	//test if left/right child exists
+	if( leftChild < len || rightChild < len ){
+		//since child exists swap index with lowest child
+		if( rightChild < len ){
+			newIndex = A[leftChild] <= A[rightChild] ? leftChild : rightChild;
+		}else{
+			newIndex = leftChild;
+		}
+		swapVal = A[newIndex];
+		A[newIndex] = A[index];
+		A[index] = swapVal;
+		//then run heapify on newly updated index
+		heapify( A, newIndex, len );
+	}
+	//else noop (we are done with this heapify)
 };
 
-void buildHeap( int A[], int n ){
+void buildHeap( int A[], int len ){
 
 };
 
 int getMin( int A[] ){
-	return 0;
+	return A[0];
 };
 
-void printList( int A[], int n ){
 
+
+void printList( int A[], int n ){
+	printf("Elements in the list: ");
+	int i;
+	for( i=0; i < n; i++ ){
+		printf("%d ", A[i] );
+	}
+	printf("\n");
 };
--- HW1-min-heap/heap.h
+++ HW1-min-heap/heap.h
@@ -3,14 +3,16 @@
  * already satisfy the min-heap property, but that A[i] may be larger than its children.
  * This function should trickle A[i] down in place such that the tree rooted at i
  * satisfies the min-heap property.
+ * @param i - Index of element to 'heapify'
+ * @param n - Length of heap
  */
-void heapify( int A[], int i, int n);
+void heapify( int A[], int index, int len );
 /**
  * Given an array A[...] of n arbitrary integers, this function will leave the
  * tree rooted at A[0] satisfying the min-heap property. Hint: Nodes
  * without children by definition trivially satisfy the min-heap property
  */
-void buildHeap( int A[], int n );
+void buildHeap( int A[], int len );
 /**
  * Assumes A[...] is a min-heap, this function returns the minimum value
  * stored in the heap
@@ -20,3 +22,12 @@ int getMin( int A[] );
  * Prints all values of the list
  */
 void printList( int A[], int n );
+
+/**
+ * Given an index in the heap, gives the location where the left child will be
+ */
+int leftChild( int index );
+/**
+ * Given an index in the heap, gives the location where the right child will be
+ */
+int rightChild( int index );
